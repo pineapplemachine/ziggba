3c572f55019a603720bbad65198f6100d6c16542
Sophie Kirschner <sophiek@pineapplemachine.com>
2025-08-26 19:12:23 +0300

    Improve new panic screen example

--- examples/panic/panic.zig
+++ examples/panic/panic.zig
@@ -2,6 +2,8 @@ const gba = @import("gba");
 
 export var header linksection(".gbaheader") = gba.Header.init("PANICEXAMPLE", "APNE", "00", 0);
 
+pub const panic = gba.debug.std_panic;
+
 pub export fn main() void {
     // Initialize the display with a solid green background.
     gba.display.ctrl.* = .initMode0(.{});
@@ -17,12 +19,12 @@ pub export fn main() void {
     var input: gba.input.BufferedKeysState = .{};
     
     // Main loop.
-    // Panic and change the background to red upon any button being pressed.
+    // Panic upon pressing the A button.
     while(true) {
         gba.bios.vblankIntrWait();
+        input.poll();
         if(input.isJustPressed(.A)) {
-            gba.display.bg_palette.colors[0] = .red;
-            // @panic("Button pressed");
+            @panic("Hello, world! This is a panic screen!");
         }
     }
 }
